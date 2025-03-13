79adf40382735699deb277523fa697e9a01e6f74
Enes Subaşı <78020226+D0vahzul@users.noreply.github.com>
2025-03-13 13:22:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 $title = "PHP - Web Site";
-echo "Hello World!sdad + asdjasd + fetch yeni + yepyeni + private";
+echo "Hello World!sdad + asdjasd + fetch yeni + yepyeni + private s";
 echo $title;
